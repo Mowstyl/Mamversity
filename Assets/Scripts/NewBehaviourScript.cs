7541002a24f8a0c4f0c8268fb50aa7a8678d223f
Termostatos <4ferluj@gmail.com>
2018-03-22 19:28:48 +0100

    We have a drunk!

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -24,45 +24,14 @@ public class NewBehaviourScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        Vector3 position = new Vector3(UnityEngine.Random.Range(-10.0f, 10.0f), 0, UnityEngine.Random.Range(-10.0f, 10.0f));
-
-        int rnd = UnityEngine.Random.Range(0, 100);
-        int rnd2 = UnityEngine.Random.Range(0, 100);
-
-
-        if (flag1)
-        {
-            
+        
             waking = Input.GetAxis("Vertical");
-            anim.SetFloat("waking", 1.0f);
-           
-
-        }
-        else
-        {
-            waking = Input.GetAxis("Vertical");
-            anim.SetFloat("waking", 0.0f);
-        }
-
-        if (rnd > 95)
-        {
-            flag1 = !flag1;
-        }
-        if (rnd2 > 95)
-        {
-            flag2 = !flag2;
-        }
-
-        if (flag2)
-        {
-            turning = 1.0f;
+            anim.SetFloat("waking", waking);
+   
+      
+            turning = Input.GetAxis("Horizontal");
             transform.Rotate(new Vector3(0.0f, turnspeed * turning * Time.deltaTime));
-        }
-        else
-        {
-            turning =-1.0f;
-            transform.Rotate(new Vector3(0.0f, turnspeed * turning * Time.deltaTime));
-        }
+      
        
 
     }
