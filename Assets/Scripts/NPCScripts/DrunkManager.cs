d0b1135df31f38833ea219e136888d3d0462f385
Termostatos <4ferluj@gmail.com>
2018-04-03 16:15:31 +0200

    Creation of drunkers and lovers

---
+++ Assets/Scripts/NPCScripts/DrunkManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrunkManager : MonoBehaviour {
+
+    public GameObject drunk;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Spawn () {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(drunk, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+}
---
+++ Assets/Scripts/NPCScripts/LoverManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoverManager : MonoBehaviour {
+
+    public GameObject lover;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+    public int numLovers = 0;
+	// Use this for initialization
+	void Start () {
+        
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+
+	}
+	
+	// Update is called once per frame
+	void Spawn () {
+        if (numLovers <= 0) { return; }
+        for (; numLovers >= 0 ; numLovers --)
+        {
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            Instantiate(lover, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        }
+	}
+}
