289e82298bd9e853050c3e1b56fe27ee25335fee
Manuel Bermúdez <manuel.009ab@gmail.com>
2018-04-18 20:12:05 +0200

    Generacion edificios y calles

---
+++ Assets/ProceduralGeneration/Building Generator/Scripts/Editor/BuildingControllerEditor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BrainyBeard.Procedural.Building.Unity
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(BuildingController))]
+    public class BuildingControllerEditor : BuildingManagerEditor
+    { protected override void PostGenerate(GameObject prefab) { } }
+}
