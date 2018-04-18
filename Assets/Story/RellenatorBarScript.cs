9cbead9808ee382319ac4fde369f3f81f7a3c827
Pandeleche <curromakinon@hotmail.com>
2018-04-18 17:58:42 +0100

    HUD and MainMenu

---
+++ Assets/Story/RellenatorBarScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RellenatorBarScript : MonoBehaviour {
+	public MainCharacter mainCharacterScript;
+	public Canvas HUD;
+	private int maximum_width = 500;
+
+	private int last_inner_peace;
+	private int last_sociability;
+	private int last_madness;
+
+	// Use this for initialization
+	void Start () {
+		Image bar;
+		bar = HUD.GetComponent<Image>();
+		RectTransform trans = bar.GetComponent<RectTransform>();
+		trans.anchorMin =(new Vector2(-100, 150));
+		trans.anchorMax =(new Vector2 (-100+(maximum_width/(mainCharacterScript.getMadness()/100)),180));		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Image bar;
+		if (mainCharacterScript.getMadness () != last_madness) {
+			bar = HUD.GetComponent<Image>();
+			RectTransform trans = bar.GetComponent<RectTransform>();
+			trans.anchorMin= (new Vector2 (-100, 150));
+			trans.anchorMax= (new Vector2 (-100+(maximum_width/(mainCharacterScript.getMadness()/100)),180));
+		}
+		
+	}
+}
