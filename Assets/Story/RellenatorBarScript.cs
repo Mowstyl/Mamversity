ddfd94a21f772f6e6ec1e581a24c8f29071ca783
Mowstyl <indikalesken@hotmail.com>
2018-05-02 20:24:34 +0200

    Fixed bars

--- Assets/Story/RellenatorBarScript.cs
+++ Assets/Story/RellenatorBarScript.cs
@@ -1,87 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RellenatorBarScript : MonoBehaviour {
 	public MainCharacter mainCharacterScript;
-	public Image barMadness;
-	public Text textMadness;
-	public Image barInnerPeace;
-	public Text textInnerPeace;
-	public Image barSociability;
-	public Text textSociability;
-	private int maximum_width = 500;
+	public Text text;
 
-	private int last_inner_peace;
-	private int last_sociability;
-	private int last_madness;
-	private RectTransform trans;
-	public Transform barPositionMadness;
-	public Transform textPositionMadness;
-	public Transform barPositionInnerPeace;
-	public Transform textPositionInnerPeace;
-	public Transform barPositionSociability;
-	public Transform textPositionSociability;
+	public RectTransform barPosition;
+	public RectTransform textPosition;
+	public Variable variable;
+
+	private int last_value;
+	private float textXOffset;
+	private float barXOffset;
+
+	private Func<int> getValue;
 
 	// Use this for initialization
 	void Start () {
-		float new_x;
-		trans = barMadness.GetComponent<RectTransform>();
-		//float new_x = barPositionMadness.position.x - (float)0.12*mainCharacterScript.getMadness();
-		//barPositionMadness.position = new Vector3(new_x,barPositionMadness.position.y,barPositionMadness.position.z);
-		new_x = textPositionMadness.position.x - (float)0.12*mainCharacterScript.getMadness();
-		textPositionMadness.position = new Vector3(new_x,textPositionMadness.position.y,textPositionMadness.position.z);
-		trans.SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, mainCharacterScript.getMadness () * 5);
-		textMadness.text = mainCharacterScript.getMadness ().ToString();
+		switch (variable) {
+		case Variable.INNER_PEACE:
+			getValue = mainCharacterScript.getInnerPeace;
+			break;
+		case Variable.SOCIABILITY:
+			getValue = mainCharacterScript.getSociability;
+			break;
+		case Variable.MADNESS:
+			getValue = mainCharacterScript.getMadness;
+			break;
+		}
 
-		trans = barInnerPeace.GetComponent<RectTransform>();
-		//new_x = barPositionInnerPeace.position.x - (float)0.12*mainCharacterScript.getInnerPeace();
-		//barPositionInnerPeace.position = new Vector3(new_x,barPositionInnerPeace.position.y,barPositionInnerPeace.position.z);
-		new_x = textPositionInnerPeace.position.x - (float)0.12*mainCharacterScript.getInnerPeace();
-		textPositionInnerPeace.position = new Vector3(new_x,textPositionInnerPeace.position.y,textPositionInnerPeace.position.z);
-		trans.SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, mainCharacterScript.getInnerPeace () * 5);
-		textInnerPeace.text = mainCharacterScript.getInnerPeace ().ToString();
-
-		trans = barSociability.GetComponent<RectTransform>();
-		//new_x = barPositionSociability.position.x - (float)0.12*mainCharacterScript.getSociability();
-		//barPositionSociability.position = new Vector3(new_x,barPositionSociability.position.y,barPositionSociability.position.z);
-		new_x = textPositionInnerPeace.position.x - (float)0.12*mainCharacterScript.getSociability();
-		textPositionSociability.position = new Vector3(new_x,textPositionSociability.position.y,textPositionSociability.position.z);
-		trans.SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, mainCharacterScript.getSociability () * 5);
-		textSociability.text = mainCharacterScript.getSociability ().ToString();
+		last_value = -1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float new_x;
-		if (mainCharacterScript.getMadness () != last_madness) {
-			//float new_x = barPositionMadness.position.x - (float)0.12*mainCharacterScript.getMadness();
-			//barPositionMadness.position = new Vector3(new_x,barPositionMadness.position.y,barPositionMadness.position.z);
-			new_x = textPositionMadness.position.x - (float)0.12*mainCharacterScript.getMadness();
-			textPositionMadness.position = new Vector3(new_x,textPositionMadness.position.y,textPositionMadness.position.z);	
-			trans.SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, mainCharacterScript.getMadness () * 5);
-			last_madness = mainCharacterScript.getMadness ();
-			textMadness.text = last_madness.ToString();
-		}
+		int value = getValue ();
 
-		if (mainCharacterScript.getInnerPeace () != last_inner_peace) {
-			//float new_x = barPositionInnerPeace.position.x - (float)0.12*mainCharacterScript.getInnerPeace();
-			//barPositionInnerPeace.position = new Vector3(new_x,barPositionInnerPeace.position.y,barPositionInnerPeace.position.z);
-			new_x = textPositionInnerPeace.position.x - (float)0.12*mainCharacterScript.getInnerPeace();
-			textPositionInnerPeace.position = new Vector3(new_x,textPositionInnerPeace.position.y,textPositionInnerPeace.position.z);
-			trans.SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, mainCharacterScript.getInnerPeace () * 5);
-			textInnerPeace.text = last_inner_peace.ToString();
-		}
+		if (value != last_value) {
+			//float new_x = (barPosition.anchoredPosition.x + barPosition.sizeDelta.x / 2) - textPosition.sizeDelta.x / 2;
+			//textPosition.position = new Vector3(textPosition.position.x - value, textPosition.position.y, textPosition.position.z);
 
-		if (mainCharacterScript.getSociability () != last_sociability) {
-			//float new_x = barPositionSociability.position.x - (float)0.12*mainCharacterScript.getSociability();
-			//barPositionSociability.position = new Vector3(new_x,barPositionSociability.position.y,barPositionSociability.position.z);
-			new_x = textPositionInnerPeace.position.x - (float)0.12*mainCharacterScript.getSociability();
-			textPositionSociability.position = new Vector3(new_x,textPositionSociability.position.y,textPositionSociability.position.z);
-			trans.SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, mainCharacterScript.getSociability () * 5);
-			textSociability.text = last_sociability.ToString();
+			barPosition.SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, value * 3);
+
+			last_value = value;
+			text.text = value.ToString() + "/" + "100";
 		}
 
 	}
+}
+
+public enum Variable {
+	INNER_PEACE, SOCIABILITY, MADNESS
 }
